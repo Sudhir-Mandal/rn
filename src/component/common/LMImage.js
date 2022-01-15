e824a0c5adffe038268b39c0488c7ab5e0f33c2d
Sudhir-Mandal <mobappssolutions141@gmail.com>
2022-01-15 11:41:10 +0530

    delete

--- src/component/common/LMImage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import FastImage from 'react-native-fast-image';
-
-export default function LMImage({...rest}) {
-    return (
-        <FastImage resizeMode={'cover'}  {...rest}/>
-    );
-}
--- src/module/language/lang.js
+++
@@ -1,4 +0,0 @@
-import {vi} from './vi';
-import {en} from './en';
-
-export const Languages = {vi: vi, en: en};
